f350c128992e3dd80dd037f77f0d2e5e9e7e3120
tomiwa <codertomiwa@gmail.com>
2023-11-13 00:36:02 -0800

    task 0

---
+++ print_37.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_37 - prints percentage sign %
+ * Return: int
+ */
+
+int print_37(void)
+{
+	_putchar(37);
+	return (0);
+}
---
+++ print_str.c
@@ -0,0 +1,31 @@
+#inlcude "main.h"
+
+/**
+ * _strlen - return length of string
+ * @str: pointer to a string
+ * Return: i
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+		;
+	return (i);
+}
+
+/**
+ * _strlenc - strlen function for constant char
+ * @str: pointer to character
+ * Return: i
+ */
+
+int _strlenc(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+		;
+	return (i);
+}
