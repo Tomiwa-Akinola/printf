3beebd3fd51c769b706cb33c2176450aa45ae80b
tomiwa <codertomiwa@gmail.com>
2023-11-14 04:02:28 -0800

    updated

--- main.h
+++ main.h
@@ -1,7 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stddef.h>
 
+int printf_char(va_list mag);
+int printf_string(va_list mag);
 int _putchar(char c);
 int _printf(const char *format, ...);
+int _strlen(char *str);
+int _strlenc(const char *str);
+int print_37(void);
 
 #endif
--- print_strlen.c
+++ print_strlen.c
@@ -1,4 +1,4 @@
-#inlcude "main.h"
+#include "main.h"
 
 /**
  * _strlen - return length of string
